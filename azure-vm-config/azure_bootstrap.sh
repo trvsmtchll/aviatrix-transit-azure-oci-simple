38bc2f3128142cbd0a2a8ad6e1f65772c39f4def
trvsmtchll <trvsmtchll@yahoo.com>
2021-03-26 13:36:30 -0400

    Fixed up sqlplus

--- azure-vm-config/azure_bootstrap.sh
+++ azure-vm-config/azure_bootstrap.sh
@@ -3,10 +3,19 @@
 sudo apt update -y
 
 #install test packages
-sudo apt-get -y install traceroute unzip build-essential git gcc iperf3 -y
+sudo apt-get -y install traceroute unzip build-essential git gcc iperf3 wget libaio1 libaio-dev -y && \
 
-git clone https://github.com/Microsoft/ntttcp-for-linux
-cd ntttcp-for-linux/src
-make; make install
+cd /home/azureuser && \
 
-cp ntttcp /usr/local/bin/
+wget https://download.oracle.com/otn_software/linux/instantclient/211000/instantclient-basic-linux.x64-21.1.0.0.0.zip && \
+wget https://download.oracle.com/otn_software/linux/instantclient/211000/instantclient-sqlplus-linux.x64-21.1.0.0.0.zip && \
+unzip instantclient-basic-linux.x64-21.1.0.0.0.zip && \
+unzip instantclient-sqlplus-linux.x64-21.1.0.0.0.zip && \
+
+echo "export LD_LIBRARY_PATH=/home/azureuser/instantclient_21_1" >> /home/azureuser/.bash_profile && \
+echo "export PATH=\$PATH:/home/azureuser/instantclient_21_1" >> /home/azureuser/.bash_profile && \
+echo "export TNS_ADMIN=/home/azureuser/instantclient_21_1/network/admin" >> /home/azureuser/.bash_profile && \
+
+chown azureuser:azureuser /home/azureuser/.bash_profile && \
+chown -R azureuser:azureuser /home/azureuser/instant* && \
+rm /home/azureuser/*.zip
